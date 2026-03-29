fe4fb878346be5dff825df8019e46c8e60b5ee38
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-30 07:18:04 +0900

    3단계 수정

--- Assets/Script/SpawnPlatform.cs
+++ Assets/Script/SpawnPlatform.cs
@@ -4,70 +4,60 @@ public class SpawnPlatform : MonoBehaviour
 {
     public GameObject[] platformPrfaps;
 
-    public int count = 4;
+    private int count;
     public float width = 8;
-
-    private float lastPlatformX;
     public float maxWidth = 15;
 
     public float ypos = -1f;
-    public float xPos = 8f;
+    public float startX = 0f;
 
     private GameObject[] platforms;
-    private int currentIndex = 0;
-
+    private float lastPlatformX;
 
     private void Awake()
     {
+        count = platformPrfaps.Length;
         platforms = new GameObject[count];
+
+        lastPlatformX = startX;
+
         for (int i = 0; i < count; i++)
         {
-            platforms[i] = Instantiate(platformPrfaps[i], transform);
-            platforms[i].SetActive(false);
+            platforms[i] = Instantiate(platformPrfaps[i % platformPrfaps.Length], transform);
+
+            float halfWidth = width / 2;
+
+            Vector2 pos = new Vector2(lastPlatformX + halfWidth, ypos);
+            platforms[i].transform.position = pos;
+
+            lastPlatformX = pos.x + halfWidth;
+
+            platforms[i].SetActive(true);
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (platforms[currentIndex].transform.position.x > Mathf.Abs(maxWidth))
+        for (int i = 0; i < platforms.Length; i++)
         {
+            // ⭐ 자식/단일 프리팹 모두 대응
+            SpriteRenderer sr = platforms[i].GetComponentInChildren<SpriteRenderer>();
 
-            Vector2 pos;
-            pos.x = xPos;
-            pos.y = ypos;
-            platforms[currentIndex].transform.position = pos;
+            // ⭐ 왼쪽 끝 기준
+            float leftEdge = sr.bounds.min.x;
 
-            platforms[currentIndex].SetActive(false);
-            platforms[currentIndex].SetActive(true);
+            if (leftEdge < -maxWidth)
+            {
+                // ⭐ 실제 크기 사용
+                float halfWidth = sr.bounds.extents.x;
 
-            lastPlatformX = pos.x;
+                Vector2 pos = new Vector2(lastPlatformX + halfWidth, ypos);
 
-            currentIndex = (int)Mathf.Repeat(currentIndex + 1, platforms.Length);
+                // 👉 부모 이동
+                platforms[i].transform.position = pos;
+
+                lastPlatformX = pos.x + halfWidth;
+            }
         }
-
-
-        //if (Time.time >= LastSpawnTime + timeSpawn)
-        //{
-        //    LastSpawnTime = Time.time;
-        //    timeSpawn = Random.Range(spawnTimeRange.x, spawnTimeRange.y);
-
-        //    Vector2 pos;
-        //    pos.x = lastPlatformX + platformWidth/2;
-        //    pos.y = ypos;
-        //    platforms[currentIndex].transform.position = pos;
-
-        //    platforms[currentIndex].SetActive(false);
-        //    platforms[currentIndex].SetActive(true);
-
-        //    lastPlatformX = pos.x;
-
-        //    currentIndex = (int)Mathf.Repeat(currentIndex + 1, platforms.Length);
-        //}
     }
 }
